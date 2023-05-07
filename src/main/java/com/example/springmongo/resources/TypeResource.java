6db135cd00021634b8b9207be2b7f16ee4ddf197
Gui8v <gui8v.rocha8@hotmail.com>
2023-05-06 22:21:24 -0300

    type endpoints findById and isert

--- src/main/java/com/example/springmongo/resources/TypeResource.java
+++ src/main/java/com/example/springmongo/resources/TypeResource.java
@@ -1,15 +1,20 @@
 package com.example.springmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.example.springmongo.service.TypeService;
 import com.example.springmongo.entities.Type;
+import com.example.springmongo.service.TypeService;
 
 @RestController
 @RequestMapping(value = "/types")
@@ -24,4 +29,18 @@ public class TypeResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Type> findById(@PathVariable Long id){
+		Type obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	} 
+	
+	@PostMapping
+	public ResponseEntity<Type> insert(@RequestBody Type obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 }
+		
--- src/main/java/com/example/springmongo/service/TypeService.java
+++ src/main/java/com/example/springmongo/service/TypeService.java
@@ -1,6 +1,7 @@
 package com.example.springmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,16 @@ public class TypeService {
 	@Autowired
 	private TypeRepository repository;
 	
-	public List<Type> findAll(){
-		
+	public List<Type> findAll(){	
 		return repository.findAll();
+	}
+	
+	public Type findById(long id) {
+		Optional<Type> obj = repository.findById(id);
+			return obj.get();
+	}
+	
+	public Type insert(Type obj) {
+		return repository.save(obj);
 	}
 }
