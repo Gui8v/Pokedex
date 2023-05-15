de225a05a51a4f212fcbf2803726321f289d44f7
Gui8v <gui8v.rocha8@hotmail.com>
2023-05-15 14:15:02 -0300

    Pokemon insert endpoint implemented

--- src/main/java/com/example/springmongo/resources/PokemonResource.java
+++ src/main/java/com/example/springmongo/resources/PokemonResource.java
@@ -1,15 +1,20 @@
 package com.example.springmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.springmongo.entities.Pokemon;
+import com.example.springmongo.entities.Type;
 import com.example.springmongo.service.PokemonService;
 
 @RestController
@@ -26,4 +31,10 @@ public class PokemonResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Pokemon> insert(@RequestBody Pokemon obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
