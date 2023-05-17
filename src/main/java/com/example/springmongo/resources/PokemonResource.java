806ddd1eb8c9580b51101757f164ada6e72121b1
Gui8v <gui8v.rocha8@hotmail.com>
2023-05-17 17:34:20 -0300

    Pokemon getById endpoint implemented

--- src/main/java/com/example/springmongo/resources/PokemonResource.java
+++ src/main/java/com/example/springmongo/resources/PokemonResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.springmongo.entities.Pokemon;
-import com.example.springmongo.entities.Type;
 import com.example.springmongo.service.PokemonService;
 
 @RestController
@@ -36,5 +36,11 @@ public class PokemonResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Pokemon> findById(@PathVariable Long id){
+		Pokemon obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
